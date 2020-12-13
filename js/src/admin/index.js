8bffd98a5cd8c43d2658759ca434edaf87e13e10
Ian Morland <ian@morland.me>
2020-12-13 15:43:10 +0000

    Prep for beta 15

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,46 +1,57 @@
-import { settings } from '@fof-components';
-
-const {
-    SettingsModal,
-    items: { StringItem, BooleanItem, NumberItem },
-} = settings;
+import app from 'flarum/app';
 
 app.initializers.add('fof-stopforumspam', () => {
-    app.extensionSettings['fof-stopforumspam'] = () =>
-        app.modal.show(SettingsModal, {
-            title: app.translator.trans('fof-stopforumspam.admin.settings.title'),
-            size: 'medium',
-            items: s => [
-                <BooleanItem setting={s} name="fof-stopforumspam.username">
-                    {app.translator.trans('fof-stopforumspam.admin.settings.username_label')}
-                </BooleanItem>,
-                <BooleanItem setting={s} name="fof-stopforumspam.ip">{app.translator.trans('fof-stopforumspam.admin.settings.ip_label')}</BooleanItem>,
-                <BooleanItem setting={s} name="fof-stopforumspam.email">{app.translator.trans('fof-stopforumspam.admin.settings.email_label')}</BooleanItem>,
-                <div className="Form-group">
-                    <label>{app.translator.trans('fof-stopforumspam.admin.settings.frequency_label')}</label>
-                    <NumberItem setting={s} name="fof-stopforumspam.frequency" simple />
-                    <br />
-                    <p className="helpText">
-                        {app.translator.trans('fof-stopforumspam.admin.settings.frequency_text')} <br />
-                        {app.translator.trans('fof-stopforumspam.admin.settings.frequency_example_text')}
-                    </p>
-                </div>,
-                app.initializers.has('fof-spamblock') ? (
-                    <div className="Form-group">
-                        <label>{app.translator.trans('fof-stopforumspam.admin.settings.api_key_label')}</label>
-                        <StringItem setting={s} name="fof-stopforumspam.api_key" simple />
-                        <br />
-                        <p className="helpText">
-                            {app.translator.trans('fof-stopforumspam.admin.settings.api_key_text')} <br />
-                            {app.translator.trans('fof-stopforumspam.admin.settings.api_key_instructions_text', {
-                                register: <a href="https://www.stopforumspam.com/forum/register.php" />,
-                                key: <a href="https://www.stopforumspam.com/keys" />,
-                            })}
-                        </p>
-                    </div>
-                ) : (
-                        []
-                    ),
-            ],
+    let set = app.extensionData.for('fof-stopforumspam');
+
+    set.registerSetting({
+        label: app.translator.trans('fof-stopforumspam.admin.settings.username_label'),
+        setting: 'fof-stopforumspam.username',
+        type: 'boolean',
+    })
+        .registerSetting({
+            label: app.translator.trans('fof-stopforumspam.admin.settings.ip_label'),
+            setting: 'fof-stopforumspam.ip',
+            type: 'boolean',
+        })
+        .registerSetting({
+            label: app.translator.trans('fof-stopforumspam.admin.settings.email_label'),
+            setting: 'fof-stopforumspam.email',
+            type: 'boolean',
+        })
+        .registerSetting({
+            label: app.translator.trans('fof-stopforumspam.admin.settings.frequency_label'),
+            setting: 'fof-stopforumspam.frequency',
+            type: 'number',
+        })
+        .registerSetting({
+            label: app.translator.trans('fof-stopforumspam.admin.settings.frequency_text'),
+            setting: 'fof-stopforumspam.hidden_label1',
+            type: 'hidden',
+        })
+        .registerSetting({
+            label: app.translator.trans('fof-stopforumspam.admin.settings.frequency_example_text'),
+            setting: 'fof-stopforumspam.hidden_label2',
+            type: 'hidden',
         });
+
+    if (app.initializers.has('fof-spamblock')) {
+        set.registerSetting({
+            label: app.translator.trans('fof-stopforumspam.admin.settings.api_key_label'),
+            setting: 'fof-stopforumspam.api_key',
+            type: 'string',
+        })
+            .registerSetting({
+                label: app.translator.trans('fof-stopforumspam.admin.settings.api_key_text'),
+                setting: 'fof-stopforumspam.hidden_label3',
+                type: 'hidden',
+            })
+            .registerSetting({
+                label: app.translator.trans('fof-stopforumspam.admin.settings.api_key_instructions_text', {
+                    register: <a href="https://www.stopforumspam.com/forum/register.php" />,
+                    key: <a href="https://www.stopforumspam.com/keys" />,
+                }),
+                setting: 'fof-stopforumspam.hidden_label4',
+                type: 'hidden',
+            });
+    }
 });
