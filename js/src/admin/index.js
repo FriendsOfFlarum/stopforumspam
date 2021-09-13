6a85e7e36d3fb3f6d7d8e706ef98cf31c65146c6
Ian Morland <ian@morland.me>
2021-09-13 13:19:47 +0100

    Update import

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,4 +1,4 @@
-import app from 'flarum/common/app';
+import app from 'flarum/admin/app';
 
 app.initializers.add('fof-stopforumspam', () => {
     let set = app.extensionData.for('fof-stopforumspam');
