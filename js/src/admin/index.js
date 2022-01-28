1883f231a0755bca70c28612064862beaf0aaed8
David Sevilla Martin <me@datitisev.me>
2022-01-28 17:31:25 -0500

    Run Prettier

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,6 +1,8 @@
-import app from 'flarum/admin/app';
-import StopForumSpamSettings from './components/StopForumSpamSettings';
+import app from "flarum/admin/app";
+import StopForumSpamSettings from "./components/StopForumSpamSettings";
 
-app.initializers.add('fof-stopforumspam', () => {
-    app.extensionData.for('fof-stopforumspam').registerPage(StopForumSpamSettings);
+app.initializers.add("fof-stopforumspam", () => {
+  app.extensionData
+    .for("fof-stopforumspam")
+    .registerPage(StopForumSpamSettings);
 });
