8796e38d83b8270ee46390e8f48d67f96b7f5b1e
David Sevilla Martin <6401250+datitisev@users.noreply.github.com>
2022-10-28 04:28:21 -0400

    fix: migrations assume flarum is already installed (#20)
    * fix: migrations assume flarum is already installed
    
    * Apply fixes from StyleCI
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- migrations/2019_09_15_000000_migrate_extension_settings.php
+++ migrations/2019_09_15_000000_migrate_extension_settings.php
@@ -9,22 +9,17 @@
  * file that was distributed with this source code.
  */
 
-use Flarum\Settings\SettingsRepositoryInterface;
 use Illuminate\Database\Schema\Builder;
 
 return [
     'up' => function (Builder $schema) {
-        /**
-         * @var SettingsRepositoryInterface
-         */
-        $settings = resolve('flarum.settings');
+        $db = $schema->getConnection();
         $keys = ['username', 'email', 'ip', 'frequency', 'api_key'];
 
         foreach ($keys as $key) {
-            if (($value = $settings->get($full = "sfs.$key")) != null) {
-                $settings->set("fof-stopforumspam.$key", $value);
-                $settings->delete($full);
-            }
+            $db->table('settings')
+                ->where('key', "sfs.$key")
+                ->update(['key' => "fof-stopforumspam.$key"]);
         }
     },
     'down' => function (Builder $schema) {
